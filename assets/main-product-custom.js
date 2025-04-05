8a49fc54b543a0ef7abea8c8d552a1d221cc881c
kjrcoder <kjrcoder@gmail.com>
2025-04-05 20:15:39 +0800

    minor

--- assets/main-product-custom.js
+++ assets/main-product-custom.js
@@ -426,7 +426,7 @@ try {
 				var affirmElement = document.querySelector('.affirm-as-low-as');
 
 				if (affirmElement) {
-					document.querySelector('.paylater-logo').innerHTML += `<img onclick="document.querySelector('.affirm-modal-trigger')?.click()" src="https://cdn.shopify.com/s/files/1/0884/2012/2940/files/affirm-logo.png?v=1743142751" width="70" height="28" style="max-width: 110px;cursor: pointer;">`
+					document.querySelector('.paylater-logo').innerHTML += `<img onclick="document.querySelector('.affirm-modal-trigger')?.click()" src="https://cdn.shopify.com/s/files/1/0884/2012/2940/files/affirm-logo.png?v=1743142751" width="65" height="24" style="max-width: 110px;cursor: pointer;margin-top:-13px;object-fit:contain;">`
 					clearInterval(affirmIntervalTrigger);
 				}
 			}, 500)
