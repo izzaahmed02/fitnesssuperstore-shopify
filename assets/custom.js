1bfc0defdef47607943b14feeb6a83ad8232315a
Izza Ahmed <161078953+izzaahmed02@users.noreply.github.com>
2026-03-12 01:32:56 +0500

    Remove remaining Globo app files and embeds

--- assets/custom.js
+++ assets/custom.js
@@ -164,7 +164,6 @@ onReady(() => {
   }, { passive: true });
 
   document.addEventListener('click', (e) => {
-    if (e.target.closest('.button.globo-formbuilder-open')) document.body.style.overflow = 'hidden';
     if (e.target.closest('.header.dismiss')) document.body.style.overflow = 'auto';
   }, { passive: true });
 
